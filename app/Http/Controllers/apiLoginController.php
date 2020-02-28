e728e6c38a7a9e0cace12b9c793597d86507a2a8
egorzhartun <egorzhartun@gmail.com>
2020-02-28 12:24:17 +0300

    changed login controller

--- app/Http/Controllers/apiLoginController.php
+++ app/Http/Controllers/apiLoginController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class apiLoginController extends Controller
 {
-    //
+	public function login() {
+		
+		$credentials = request(['email', 'password']);
+
+		if (!$token = auth('api')->attempt($credentials)) {
+			return response()->json(['error' => 'Unauthorized'], 401);
+		}
+
+		return response()->json([
+
+			'token' => $token,
+			'type' => 'bearer',
+			'expires' => auth('api')->factory()->getTTL() * 60,
+
+		]);
+	}
 }
